a290e49669756b3fafbaee36d1f0edb8af91fb21
DeshanHarshana <nawarathnadeshan@gmail.com>
2021-05-13 09:13:20 +0530

    path set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,30 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'my-listings',
+    loadChildren: () => import('./pages/my-listings/my-listings.module').then( m => m.MyListingsPageModule)
+  },
+  {
+    path: 'my-listings-add',
+    loadChildren: () => import('./pages/my-listings-add/my-listings-add.module').then( m => m.MyListingsAddPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   }
 ];
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAiqlX6RT2GA6IWlriFR1LbASA9RpnHsfk",
+    authDomain: "mobile-marketplace-22bc0.firebaseapp.com",
+    projectId: "mobile-marketplace-22bc0",
+    storageBucket: "mobile-marketplace-22bc0.appspot.com",
+    messagingSenderId: "729229353546",
+    appId: "1:729229353546:web:700cab43e12dc8752d7cb3",
+    measurementId: "G-WRE32NCPH1"
+  }
 };
 
 /*
